c84ee84296b20b35feb4867ec066c2f4477599b8
dashuncel <korovkinada@ya.ru>
2017-07-14 18:20:07 +0800

    v2

--- classes.php
+++ classes.php
@@ -13,76 +13,120 @@
 
     function __construct ($brand, $model, $color) {
       // прочие характеристики в функицональной зависимости от первых двух, можно получать данные запросом куда-то
-      $this->brand = $brand;
-      $this->model = $model;
-      $this->color = $color;
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->color = $color;
 
-      echo "Создан автомобиль $this->brand  $this->model, цвет $color";
-      $this->loadCharaters();
+        echo 'Создан автомобиль '.$this->getBrand().' '.$this->getModel().' цвет $color';
+        $this->loadCharaters();
     }
 
-    private function loadCharaters() {
+      public function getModel()
+      {
+          return $this->model;
+      }
+
+      public function setModel($model)
+      {
+          $this->model = $model;
+          return $this;
+      }
+
+      private function loadCharaters()
+      {
       // функция от $this->brand, $this->model
-    }
+      }
 
-    public function __get($property)
-    {
-        switch ($property)
-        {
-          case 'brand':
-            return $this->brand;
-            break;
- 
-          case 'consumption':
-            return $this->consumption;
-            break;
+      public function getBrand()
+      {
+          return $this->brand;
+      }
 
-          case 'power':
-            return $this->power;
-            break;  
+      public function setBrand($brand)
+      {
+          $this->brand = $brand;
+      }
 
-          // итп  
-        }
-    }
- 
-    public function __set($property, $value)
-    {
-        switch ($property)
-        {
-          case 'brand':
-            $this->brand = $value;
-            break;
- 
-          case 'consumption':
-            $this->consumption = $value;
-            break;
+      public function getConsumption()
+      {
+          return $this->consumption;
+      }
 
-          case 'power':
-            $this->power = $value;
-            break;  
+      public function setConsumption($consumption)
+      {
+          $this->consumption = $consumption;
+      }
 
-        }
-        echo "Для машины $this->brand $this->model установлено свойство '$property' = $value<br/>";
-    }
+      public function getBody()
+      {
+          return $this->body;
+      }
+
+      public function setBody($body)
+      {
+          $this->body = $body;
+      }
+
+      public function getColor()
+      {
+          return $this->color;
+      }
+
+      public function setColor($color)
+      {
+          $this->color = $color;
+      }
+
+      public function getPower()
+      {
+          return $this->power;
+      }
+
+      public function setPower($power)
+      {
+          $this->power = $power;
+      }
+
+      public function getDimensions()
+      {
+          return $this->dimensions;
+      }
+
+      public function setDimensions($dimensions)
+      {
+          $this->dimensions = $dimensions;
+      }
+
+      public function getSpeed()
+      {
+          return $this->speed;
+      }
+
+      public function setSpeed($speed)
+      {
+          $this->speed = $speed;
+      }
+
+
 
     public function start() {
     	$this->speed = 100;
-    	echo "Машина $this->brand $this->model поехала со скоростью $this->speed км/ч<br/>";
+    	echo "Машина ".$this->brand.' '.$this->model.' поехала со скоростью '.$this->speed.' км/ч<br/>';
     } 
 
     public function stop() {
-    	echo 'Тормозим';
+    	echo "Машина ".$this->brand.' '.$this->model.' тормозит <br/>';
     	$this->speed = 0;
     } 
 
-    public function turn() {
-    	echo 'Поворот';
-    } 
+    public function turn($side) {
+        echo "Машина ".$this->brand.' '.$this->model." поворачивает $side <br/>";
+    }
   }
 
 //----------ТЕЛЕВИЗОР: ---------------------------
-  class Tv
-  {
+class Tv
+{
     private $size;
     private $brand;
     private $model;
@@ -94,48 +138,68 @@
       // прочие характеристики частично в функицональной зависимости от первых двух
       $this->brand = $brand;
       $this->model = $model;
-      echo "Создан TV $this->brand  $this->model<br/>";
-
+      echo 'Создан TV '.$this->getBrand().' '.$this->getModel().' <br/>';
       $this->loadCharaters();
     }
 
-     public function __get($property)
+    public function getSize()
     {
-        switch ($property)
-        {
-          case 'brand':
-            return $this->brand;
-            break;
- 
-          case 'consumption':
-            return $this->brightness;
-            break;
-
-          case 'resolution':
-            return $this->resolution;
-            break;  
-          // итп  
-        }
+        return $this->size;
     }
- 
-    public function __set($property, $value)
-    {
-        switch ($property)
-        {
-          case 'brand':
-            $this->brand = $value;
-            break;
- 
-          case 'brightness':
-            $this->brightness = $value;
-            break;
 
-          case 'resolution':
-            $this->resolution = $value;
-            break;  
-         // итп
-        }
-        echo "Для TV $this->brand $this->model установлено свойство '$property' = $value<br/>";
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getBrightness()
+    {
+        return $this->brightness;
+    }
+
+    public function setBrightness($brightness)
+    {
+        $this->brightness = $brightness;
+    }
+
+    public function getResolution()
+    {
+        return $this->resolution;
+    }
+
+    public function setResolution($resolution)
+    {
+        $this->resolution = $resolution;
+    }
+
+    public function getCurrentChannel()
+    {
+        return $this->current_channel;
+    }
+
+    public function setCurrentChannel($current_channel)
+    {
+        $this->current_channel = $current_channel;
     }
 
     private function loadCharaters() {
@@ -143,43 +207,40 @@
     }
 
     public function turnOn() {
-      echo "Телевизор  $this->brand $this->model включен<br/>";
+      echo 'Телевизор  '.$this->brand.' '.$this->model." включен<br/>";
     }
 
     public function turnOff() {
-      echo "Телевизор  $this->brand $this->model выключен<br/>";
+      echo 'Телевизор '.$this->brand.' '.$this->model." выключен<br/>";
     }
 
     public function switchChanal() {
-       $this->current_channel +=  $this->$current_channel;
-       echo "Текущий канал ".$this->$current_channel;
+       $this->current_channel +=  $this->current_channel;
+       echo 'Текущий канал '.$this->current_channel;
     }
 
     public function upgradePo() {
-      echo "Обновление програмного обеспечения на телевизоре $this->brand $this->model<br/> ";
+      echo "Обновление програмного обеспечения на телевизоре ".$this->brand.' '.$this->model." <br/> ";
     }
-  }
+}
 
 //---------- РУЧКА: ---------------------------
   class Pen
   {
     private $color; // цвет стержня
-    private $size; //габариты
-    private $producter; //производитель
-    private $matherial; // материал корпуса
 
     function __construct ($color) {
       $this->color = $color;
-      echo "Куплена ручка с цветом $this->color<br/>";
+      echo "Куплена ручка с цветом ".$this->getColor()." <br/>";
     }
 
-    public function print($str) {
+    public function printColor($str) {
        echo "Ручка пишет строку $str<br/>";
     }
 
     public function setColor($color) { // замена стержня
        $this->color = $color;
-       echo "Ручка теперь пишет $color цветом<br/>";
+       echo "Ручка теперь пишет ".$this->getColor()." цветом<br/>";
        return $this;
     }
 
@@ -198,15 +259,15 @@
 
     function __construct ($poroda) {
       $this->poroda = $poroda;
-      echo "Родилась утка породы $this->poroda<br/>";
+      echo "Родилась утка породы ".$this->getPoroda()."<br/>";
     }
 
     public function fly() {
-      echo "Утка $poroda полетела<br/>";
+      echo "Утка ".$this->getPoroda()."полетела<br/>";
     }
 
     public function swim() {
-      echo "Утка $poroda поплыла<br/>";
+      echo "Утка ".$this->getPoroda()." поплыла<br/>";
     }
 
     public function eat($eda) {
@@ -222,78 +283,102 @@
           break;
       }    	
     }
+
+      public function getPoroda()
+      {
+          return $this->poroda;
+      }
+
+      public function setPoroda($poroda)
+      {
+          $this->poroda = $poroda;
+          return $this;
+      }
+
+      public function getSound()
+      {
+          return $this->sound;
+      }
+
+      public function setSound($sound)
+      {
+          $this->sound = $sound;
+          return $this;
+
+      }
+
+      public function getArea()
+      {
+          return $this->area;
+      }
+
+      public function setArea($area)
+      {
+          $this->area = $area;
+          return $this;
+      }
   }
 
 //---------- ТОВАР: ---------------------------
   class Product
   {
-    private $name; // наименование
-    private $price; // цена
-    private $category; // категория
-    private $photo; // URL фото 
-    private $unit; // единица измерения
+      private $name; // наименование
+      private $price; // цена
+      private $category; // категория
+      private $photo; // URL фото
+      private $unit; // единица измерения
 
-    public function __construct($name, $price, $category, $unit)
-    {
-      $this->name = $name;
-      $this->price = $price;
-      $this->category = $category;
-      $this->unit = $unit;
-      echo "Создан товар $this->name с базовой ценой $this->price в категории '$this->category' и единицей измерения $this->unit<br/>";
-    }
-
-    public function __get($property)
-    {
-      switch ($property)
+      public function __construct($name, $price, $category, $unit)
       {
-        case 'price':
-          return $this->price;
-          break;
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+        $this->unit = $unit;
+        echo 'Создан товар '.$this->getName().' с базовой ценой '.$this->getPrice(10).' в категории '.$this->getCategory().' и единицей измерения '.$this->getUnit()."<br/>";
+      }
 
-        case 'category':
+      public function putBasket() {
+          echo 'Товар '.$this->getName().' поместили в корзину';
+      }
+
+      public function getPrice($discount) {
+          $price = $this->price  - $this->price * $discount / 100 ;
+          return $price;
+      }
+
+      public function getName() {
+         return $this->name ;
+      }
+
+      public function getCategory()
+      {
           return $this->category;
-          break;
-
-        case 'unit':
-          return $this->unit;
-          break;  
-        // итп  
       }
-    }
- 
-    public function __set($property, $value)
-    {
-      switch ($property)
+
+      public function setCategory($category)
       {
-        case 'price':
-          $this->price = $value;
-          break;
-
-        case 'category':
-          $this->category = $value;
-          break;
-
-        case 'unit':
-          $this->unit = $value;
-          break;  
-       // итп
+          $this->category = $category;
       }
-      echo "Для товар $this->name установлено свойство '$property' = $value<br/>";
-    }
 
-    public function putBasket() {
-        echo "Товар $name поместили в корзину";
-ж   }
+      public function getPhoto()
+      {
+          return $this->photo;
+      }
 
-    public function getPrice($discount) {
-      $price = $this->price  - $this->price * $discount / 100 ;
-      return $price;
-    }
+      public function setPhoto($photo)
+      {
+          $this->photo = $photo;
+      }
 
-    public function getName() {
-      return $this->name ;
-    }
+      public function getUnit()
+      {
+          return $this->unit;
+      }
 
+      public function setUnit($unit)
+      {
+          $this->unit = $unit;
+      }
   }
 
   //машины 
